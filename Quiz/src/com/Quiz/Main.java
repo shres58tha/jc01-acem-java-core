ea0f50c9c56be3eabe537fc8cf7e5b4e03c6f81a
shres58tha <umesh.acem@gmail.com>
2022-07-11 19:23:56 +0545

    minor bug fixed and the ans made correct

--- Quiz/src/com/Quiz/Main.java
+++ Quiz/src/com/Quiz/Main.java
@@ -4,15 +4,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int currentScore = 0;
-        int maxScore = 0;
         Scanner myObj = new Scanner(System.in);
         Question myQuestion = new Question();
         Score score = new Score(); 
-        
+ 
         while (true) {
-
-            System.out.println("Lets learn Java Array by Quiz.\n Menu");
+            System.out.println("\n\nLets learn Java Array by Quiz.\n Menu");
             System.out.println("1. Start to Learn");
             System.out.println("2. Max. Score");
             System.out.println("3. Exit");
@@ -21,11 +18,11 @@ public class Main {
             switch (userOption) {
                 case "1":
                     myQuestion.questions();
-                    System.out.println("Your Score is: " + score.getScore());
-                    System.out.println("Max Score is : " + score.getMaxScore());
+                    System.out.println( "\n" + score.getName() +"\nThank you..\n   Your Score: " + score.getScore() + "\nTime duration: " + score.getTimeStamp());
                     break;
                 case "2":
-                    System.out.println("Current Max. score  :" + score.getMaxScore());
+                    System.out.println("\nCurrent Max. score  :" + score.getMaxScore() + "\nCurrent Record Holder : " + score.getMaxScoreName()
+                    + "\nCurrent min time : " + score.getMinTime());
                     break;
                 case "3":
                     System.out.println("\nHOPE you got some Java Array Fluency.\n");
--- Quiz/src/com/Quiz/Score.java
+++ Quiz/src/com/Quiz/Score.java
@@ -2,17 +2,48 @@ package com.Quiz;
 import java.text.NumberFormat;
 
 public class Score {
-    int maxScore=0;
-    int score;
+    static int maxScore=0;         //need to be acessed from every object created
+    static String maxScoreName;
+    static String name;
+    static long timeStamp;
+    static long minTime=0;
+    static int score;
+
+    public long getMinTime() {
+        return minTime;
+    }
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+    public String getMaxScoreName() {
+        return maxScoreName;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
     public int getMaxScore() {
         return maxScore;
     }
     public int getScore() {
         return score;
     }
-    public void score(int correctAnswers, int totalQuestions){
-        score = ((int)((double) correctAnswers*100 / (double) totalQuestions ));
-        if (maxScore>score)
+    public void score(int correctAnswers, int totalQuestions){  // calculates score display score and sets the record
+        score = ( correctAnswers*100 / totalQuestions );
+        System.out.println("\n\n Your score :" + score + "\nYour Time Stamp :" + timeStamp + "\n----------------------------------\n");
+        if (maxScore<score){
             maxScore=score;
+            maxScoreName=name;
+            minTime = timeStamp;
+        }
+        if (maxScore == score && minTime > timeStamp){
+            maxScoreName = name;
+            minTime=timeStamp;
+        }
     }
 }
